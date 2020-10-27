33d0c98e879ece97bb971ccbad142a5d1da26679
Luke Trujillo <lrtrujillo@wpi.edu>
2020-10-27 10:57:45 -0400

    added assignment model, factory, migration, and seeder

--- database/migrations/2020_10_27_144316_create_assignments_table.php
+++ database/migrations/2020_10_27_144316_create_assignments_table.php
@@ -14,7 +14,20 @@ class CreateAssignmentsTable extends Migration
     public function up()
     {
         Schema::create('assignments', function (Blueprint $table) {
+            //Basic information
             $table->id();
+            $table->string("name")
+            $table->text("description")->nullable()
+            $table->string("link")->nullable()
+
+            //Course infomration
+            $table->integer("course_id")
+
+            //Assignment Due Date
+            $table->timestamp("due_date")->nullable()
+            $table->timestamp("available_until")->nullable()
+
+            //Just for record keeping
             $table->timestamps();
         });
     }
